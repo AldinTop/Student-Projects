9a3f6817070f9a0061c6688556f9ad7c01d3f2e4
AldinTop <159993617+AldinTop@users.noreply.github.com>
2024-03-04 01:56:05 -0600

    Create Student.h

---
+++ BinarySearchTree/Student.h
@@ -0,0 +1,156 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+using namespace std;
+
+//*****************************************************************************************************
+
+struct Student
+{
+	int id;
+	char name[50];
+	char citystate[50];
+	char phone[12];
+	char gender;
+	int year;
+	int credits;
+	float gpa;
+	char major[6];
+
+	Student();
+	friend ostream & operator << (ostream & out, const Student & data);
+	bool operator == (const Student & rhs) const;
+	bool operator == (int value) const;
+	Student & operator = (int value);
+	bool operator != (const Student & rhs) const;
+	bool operator != (int value) const;
+	bool operator < (const Student & rhs) const;
+	bool operator < (int value) const;
+	bool operator > (const Student & rhs) const;
+	bool operator > (int value) const;
+	bool operator <= (const Student & rhs) const;
+	bool operator <= (int value) const;
+	bool operator >= (const Student & rhs) const;
+	bool operator >= (int value) const;
+	int operator % (int value) const;
+};
+
+//*****************************************************************************************************
+
+Student::Student()
+{
+	id = 0;
+}
+
+//*****************************************************************************************************
+
+ostream & operator << (ostream & out, const Student & data)
+{
+	out << "ID:" << data.id << " |Name: " << data.name << " |Gender: " << data.gender
+		<< " |Major: " << data.major << endl;
+
+	return out;
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator == (const Student & rhs) const
+{
+	return (this->id == rhs.id);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator == (int value) const
+{
+	return (this->id == value);
+}
+
+//*****************************************************************************************************
+
+Student & Student :: operator = (int value)
+{
+	this->id = value;
+	return *this;
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator != (const Student & rhs) const
+{
+	return (this->id != rhs.id);
+}
+
+//*****************************************************************************************************
+
+int Student :: operator % (int value) const
+{
+	return (this->id % value);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator != (int value) const
+{
+	return (this->id != value);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator < (const Student & rhs) const
+{
+	return (this->id < rhs.id);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator < (int value) const
+{
+	return (this->id < value);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator > (const Student & rhs) const
+{
+	return (this->id > rhs.id);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator > (int value) const
+{
+	return (this->id > value);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator <= (const Student & rhs) const
+{
+	return (this->id <= rhs.id);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator <= (int value) const
+{
+	return (this->id <= value);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator >= (const Student & rhs) const
+{
+	return (this->id >= rhs.id);
+}
+
+//*****************************************************************************************************
+
+bool Student :: operator >= (int value) const
+{
+	return (this->id >= value);
+}
+
+//*****************************************************************************************************
+
+#endif
